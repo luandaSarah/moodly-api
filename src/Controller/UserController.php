f2f36dcf2638d768125bc2e2ca5626b19e652e14
kaimon0 <luandasarah1031@gmail.com>
2025-05-28 14:33:52 +0200

    feat(security) clés jwt générés, firewall et acces_controller configuré, login user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('api/users', name: 'api_users')]
+class UserController extends AbstractController
+{
+    
+}
